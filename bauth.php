7046ad80a29ba9a1d4a1da7919a73c863c30a3d5
chenyinl <linchenyin@gmail.com>
2015-08-01 16:01:56 -0700

    Update bauth.php
    If somehow using .htaccess or apache config is not allowed, use this php file will do the basic auth

--- bauth.php
+++ bauth.php
@@ -1,3 +1,4 @@
+<?php
 if (!isset($_SERVER['PHP_AUTH_USER']) || !pc_validate($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW'])) {
     header('WWW-Authenticate: Basic realm="Restricted Area"');
     header('HTTP/1.0 401 Unauthorized');
@@ -10,7 +11,7 @@ function pc_validate($user,$pass) {
     $users = array(
         'clin' => '7b84e255fbd2727e2034b72f020badf3',
         'nkim'  => '86a375aacb17606c185d31c8d3e320ce',
-        'tbuser' => 'ccfd16633f8033d2d2cdee9f44bbe273'
+        'user3' => 'ccfd16633f8033d2d2cdee9f44bbe273'
     );
     if (isset($users[$user]) && ($users[$user] == md5($pass) )) {
         return true;
